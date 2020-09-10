65b1257952675b6b0ba90694e7c40116b2cd7ea9
User <Leha-5@mail.ru>
2020-09-10 09:14:22 +0400

    Lesson2CorrectionTask6

--- src/task6.java
+++ src/task6.java
@@ -4,18 +4,25 @@ import java.io.InputStreamReader;
 public class task6 {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         int number = Integer.parseInt(reader.readLine());
-        if (number == 0) {
-            System.out.println("ноль");
-        } else if (number < 0 && number % 2 == 0) {
-            System.out.println("отрицательное четное число");
-        } else if (number < 0 && number % 2 == 1) {
-            System.out.println("отрицательное нечетное число");
-        } else if (number > 0 && number % 2 == 0) {
-            System.out.println("положительное четное число");
-        } else if (number > 0 && number % 2 == 1) {
-            System.out.println("положительное нечетное число");
+        String tmp1 = null, tmp2 = null, result;
+
+        if (number > 0) {
+            tmp1 = "Положительное ";
+        } else if (number < 0) {
+            tmp1 = "Отрицательное ";
         }
+        result = tmp1;
+        if (number % 2 == 0 && number != 0) {
+            tmp2 = "четное";
+        } else if (number % 2 != 0) {
+            tmp2 = "нечетное";
+        }
+        result += tmp2;
+        if (number == 0) {
+            result = "нулевое число";
+        }
+        System.out.println(result);
     }
 }
-
